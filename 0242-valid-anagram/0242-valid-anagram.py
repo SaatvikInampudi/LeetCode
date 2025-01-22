9b5a0b0501355c70591e33b4c9acd7ecc5dbeb82
Saatvik Inampudi <isaatvik@gmail.com>
2025-01-22 09:04:58 -0500

    Time: 13 ms (51.1%), Space: 18 MB (37.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Check if lengths are the same; if not, they can't be anagrams
-        if len(s) != len(t):
-            return False
+        dict_s = {}
+        dict_t = {}
+
+        for char in s:  # Iterate over the original string, not the set
+            dict_s[char] = dict_s.get(char, 0) + 1  # Increment count for each character
         
-        # Sort both strings and compare them
-        return sorted(s) == sorted(t)
+        for char in t:
+            dict_t[char] = dict_t.get(char,0) + 1
+
+        return dict_s == dict_t
