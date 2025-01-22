960e137dc5f67400d90c19bec5d8a5cb6b20704d
Saatvik Inampudi <isaatvik@gmail.com>
2025-01-22 09:10:17 -0500

    Time: 7 ms (90.37%), Space: 17.9 MB (47.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict_s = {}
-        dict_t = {}
+        # dict_s = {}
+        # dict_t = {}
 
-        for char in s:  # Iterate over the original string, not the set
-            dict_s[char] = dict_s.get(char, 0) + 1  # Increment count for each character
+        # for char in s:  # Iterate over the original string, not the set
+        #     dict_s[char] = dict_s.get(char, 0) + 1  # Increment count for each character
         
-        for char in t:
-            dict_t[char] = dict_t.get(char,0) + 1
+        # for char in t:
+        #     dict_t[char] = dict_t.get(char,0) + 1
 
-        return dict_s == dict_t
+        # return dict_s == dict_t
+        return Counter(s) == Counter(t)
