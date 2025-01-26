34b6a7d5091ef5c48766f590733076fc3aeb721c
Saatvik Inampudi <isaatvik@gmail.com>
2025-01-26 16:54:17 -0500

    Time: 0 ms (100%), Space: 17.9 MB (23.76%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # to_rem = set()
         k = 0
         for i in range(len(nums)):
             if val != nums[i]:
-                # to_rem.add(i)
+                nums[k] = nums[i]
                 k+=1
-        nums.sort(key = lambda x: x==val)
-        # for i in to_rem:
-        #     nums.pop(i)
+        # nums.sort(key = lambda x: x==val)
         print(k)
         return k
                 
