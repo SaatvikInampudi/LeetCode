df8aaad3a7711866f67fe81215c44d97625de5c3
Saatvik Inampudi <isaatvik@gmail.com>
2025-02-04 18:51:07 -0500

    Time: 0 ms (100%), Space: 17.9 MB (10.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,23 +4,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # # heapq.heapify(nums)
-        # # print(Counter(nums).get(0))
-        # # nums = [0 for i in range(Counter(nums).get(0))]
-        
-        counter = Counter(nums)
-        x = counter.get(0, 0)
-        y = counter.get(1, 0)
-        z = counter.get(2, 0)
+        low, mid, high = 0, 0, len(nums) - 1
 
-        # Modify nums in-place
-        index = 0
-        for _ in range(x):
-            nums[index] = 0
-            index += 1
-        for _ in range(y):
-            nums[index] = 1
-            index += 1
-        for _ in range(z):
-            nums[index] = 2
-            index += 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
