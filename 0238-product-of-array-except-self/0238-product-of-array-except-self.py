f2e9f7780a8dc3c7bda1e010e090dfd540ee16bd
Saatvik Inampudi <isaatvik@gmail.com>
2025-02-10 07:38:07 -0500

    Time: 20 ms (75.78%), Space: 23.3 MB (53.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -10,38 +10,38 @@ class Solution:
         #         nums[i] = temp
         # return ans
 
-        prefix = [1 for i in range(len(nums))]
-        prefix[0] = nums[0]
-        suffix = [1 for i in range(len(nums))]
-        suffix[len(nums)-1] = nums[len(nums)-1]
-        for i in range(1,len(nums)):
-            prefix[i] = prefix[i-1]*nums[i]
+        # prefix = [1 for i in range(len(nums))]
+        # prefix[0] = nums[0]
+        # suffix = [1 for i in range(len(nums))]
+        # suffix[len(nums)-1] = nums[len(nums)-1]
+        # for i in range(1,len(nums)):
+        #     prefix[i] = prefix[i-1]*nums[i]
 
-        for j in range(len(nums)-2,-1,-1):
-            suffix[j] = suffix[j+1]*nums[j]
+        # for j in range(len(nums)-2,-1,-1):
+        #     suffix[j] = suffix[j+1]*nums[j]
         
-        ans= [1 for i in range(len(nums))]
-        # print(ans)
-        ans[0] = suffix[1]
-        ans[len(nums)-1] = prefix[len(nums)-2]
-        for i in range(1,len(nums)-1):
-            ans[i] = prefix[i-1]*suffix[i+1]
-            # print(ans)
-        return ans
-
-        # n = len(nums)
-        # ans = [1] * n  # Initialize answer array with 1
-
-        # # Compute prefix products in ans array
-        # prefix = 1
-        # for i in range(n):
-        #     ans[i] = prefix
-        #     prefix *= nums[i]
-
-        # # Compute suffix products directly in ans array
-        # suffix = 1
-        # for i in range(n-1, -1, -1):
-        #     ans[i] *= suffix
-        #     suffix *= nums[i]
-
+        # ans= [1 for i in range(len(nums))]
+        # # print(ans)
+        # ans[0] = suffix[1]
+        # ans[len(nums)-1] = prefix[len(nums)-2]
+        # for i in range(1,len(nums)-1):
+        #     ans[i] = prefix[i-1]*suffix[i+1]
+        #     # print(ans)
         # return ans
+
+        n = len(nums)
+        ans = [1] * n  # Initialize answer array with 1
+
+        # Compute prefix products in ans array
+        prefix = 1
+        for i in range(n):
+            ans[i] = prefix
+            prefix *= nums[i]
+
+        # Compute suffix products directly in ans array
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            ans[i] *= suffix
+            suffix *= nums[i]
+
+        return ans
