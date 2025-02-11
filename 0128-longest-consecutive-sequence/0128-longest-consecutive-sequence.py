122c72661dd186f12ff6eb1cdd0f64406c8f0ec3
Saatvik Inampudi <isaatvik@gmail.com>
2025-02-11 03:23:04 -0500

    Time: 55 ms (46.77%), Space: 31.8 MB (96.1%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,22 +2,40 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if nums == []:
             return 0
-        minheap = []
-        for i in nums:
-            heapq.heappush(minheap, i)
-        sorted_list = [heapq.heappop(minheap) for _ in range(len(minheap))]
-        sorted_list = list(dict.fromkeys(sorted_list))
-        print(sorted_list)
+        # minheap = []
+        # for i in nums:
+        #     heapq.heappush(minheap, i)
+        # sorted_list = [heapq.heappop(minheap) for _ in range(len(minheap))]
+        # sorted_list = list(dict.fromkeys(sorted_list))
+        # print(sorted_list)
+        # ans = 1
+        # max_ans = 0
+        # for i in range(1,len(sorted_list)):
+        #     if sorted_list[i] == sorted_list[i-1]+1:
+        #         ans+=1
+        #     else:
+        #         if ans>max_ans:
+        #             max_ans = ans
+        #         ans = 1
+        # if ans>max_ans:
+        #         max_ans = ans
+        # return max_ans
+
+
+        nums.sort()
 
         ans = 1
         max_ans = 0
-        for i in range(1,len(sorted_list)):
-            if sorted_list[i] == sorted_list[i-1]+1:
-                ans+=1
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                pass
             else:
-                if ans>max_ans:
-                    max_ans = ans
-                ans = 1
+                if nums[i] == nums[i-1]+1:
+                    ans+=1
+                else:
+                    if ans>max_ans:
+                        max_ans = ans
+                    ans = 1
         if ans>max_ans:
                 max_ans = ans
         return max_ans
