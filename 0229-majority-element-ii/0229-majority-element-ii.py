b447772f440f6939f9658ca35c5b89ef93fef8fc
Saatvik Inampudi <isaatvik@gmail.com>
2025-02-11 21:18:36 -0500

    Time: 22 ms (5.2%), Space: 19.1 MB (39.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        c = Counter(nums)
+        # c = Counter(nums)
         ans = []
-        for i in range(len(c.most_common(len(nums)))):
-            if c.most_common(len(nums))[i][1] > math.floor(len(nums)/3):
-                ans.append(c.most_common(len(nums))[i][0])
+        for i in range(len(Counter(nums).most_common(len(nums)))):
+            if Counter(nums).most_common(len(nums))[i][1] > math.floor(len(nums)/3):
+                ans.append(Counter(nums).most_common(len(nums))[i][0])
         return ans
