8ac75693bceeac8b3f9c7f888b0c4191dc1e8168
Saatvik Inampudi <isaatvik@gmail.com>
2025-03-28 17:20:39 -0400

    Time: 42 ms (91.07%), Space: 34.2 MB (53.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -22,20 +22,39 @@ class Solution:
         # return max_ans
 
 
-        nums.sort()
+        # nums.sort()
 
-        ans = 1
-        max_ans = 0
-        for i in range(1,len(nums)):
-            if nums[i] == nums[i-1]:
-                pass
-            else:
-                if nums[i] == nums[i-1]+1:
-                    ans+=1
-                else:
-                    if ans>max_ans:
-                        max_ans = ans
-                    ans = 1
-        if ans>max_ans:
-                max_ans = ans
-        return max_ans
+        # ans = 1
+        # max_ans = 0
+        # for i in range(1,len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         pass
+        #     else:
+        #         if nums[i] == nums[i-1]+1:
+        #             ans+=1
+        #         else:
+        #             if ans>max_ans:
+        #                 max_ans = ans
+        #             ans = 1
+        # if ans>max_ans:
+        #         max_ans = ans
+        # return max_ans
+        # Create a set of nums for O(1) lookups
+        
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            # Only start counting if 'num' is the beginning of a sequence
+            if num - 1 not in numSet:
+                currentNum = num
+                currentStreak = 1
+
+                # Count the consecutive numbers following num
+                while currentNum + 1 in numSet:
+                    currentNum += 1
+                    currentStreak += 1
+
+                longest = max(longest, currentStreak)
+                
+        return longest
