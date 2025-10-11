207da12636d139999010e855f7616e9c9a9bf0a4
Saatvik Inampudi <isaatvik@gmail.com>
2025-10-11 19:56:54 -0400

    Time: 2 ms (30.18%), Space: 17.9 MB (30.45%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,58 +1,33 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # for i in range(len(strs)):
-        #     for k in range(i+1,len(strs)):
-        #         if len(strs[i])<=len(strs[k]):
-        #             for j in range(1,len(strs[k])):
-        #                 if (strs[i][:j] == strs[k][:j]):
-        #                     ans = strs[i][:j]
-        #                 else:
-        #                     ans = ""
-        #         else:
-        #             for j in range(1,len(strs[i])):
-        #                 if strs[i][:j] == strs[k][:j]:
-        #                     ans = strs[i][:j]
-        #                 else:
-        #                     ans = ""
-        # print(len(strs))
-        if len(strs) > 1 :  
-            check = 0
-            # max_len = 0
-            # for x in range(len(strs)):
-            #     if len(strs[x]) > max_len:
-            #         max_len = len(strs[x])
-
-            lcp = strs[0][:1]
-            # print('max=',max_len)
-            # print(lcp)
-            if len(strs[0]) != 0:
-                # print('chk')
-                for j in range(1,len(strs[0])+1):
-                    # print('j',j)
-                    for i in range(1,len(strs)):
-                        # print('i',i)
-                        # print(strs[i][:j])
-                        if (strs[i][:j] == lcp):
-                            # print('chk2')
-                            if i == len(strs)-1 and j == len(strs[0]):
-                                return lcp
-                            continue
-                        else:
-                            # print('check')
-                            check = 1
-                            if len(lcp[:len(lcp)-1]) != 0:
-                                # print('testing',len(lcp[:len(lcp)-1]))
-                                return lcp[:len(lcp)-1]
-                            else:
-                                return ""
-                            break
-                    # print('before incr=',lcp)
-                    lcp = strs[0][:j+1]
-                    # print('after incrementing lcp=',lcp)
-                    if check == 1:
-                        break
-            else:
-                return ""
-        else:
-            # print('test')
+        # print(type(strs[0][0]))
+        pref = ""
+        itr = ""
+        minstr = len(strs[0])
+        if len(strs) == 1:
             return strs[0]
+        for i in range(1,len(strs)):
+            # print(i,len(strs)-1)
+            # print(minstr,"i=",i)
+            # print(len(strs[i]),"i=",i)
+            if len(strs[i])< minstr:
+                minstr = len(strs[i]) 
+        for j in range(minstr):
+            # print(j)
+            for i in range(1,len(strs)):
+                # print(i,j,len(strs)-1)
+                # print(strs[i][j],strs[i-1][j])
+                if strs[i][j] == strs[i-1][j]:
+                    itr = strs[i][j]
+                    # print(i,j,itr)
+                else:
+                    # print(i,j,itr)
+                    itr = ""
+                    # print(i,j)
+                    break
+            # print(i,j,itr)
+            if itr != "":
+                pref += itr
+            else: break
+        return pref
+                
