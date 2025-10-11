9914674d5a0a50fb8d0fd8f5ec9fd3054457b63b
Saatvik Inampudi <isaatvik@gmail.com>
2025-10-11 19:57:47 -0400

    Time: 0 ms (100%), Space: 18 MB (30.45%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,31 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # print(type(strs[0][0]))
         pref = ""
         itr = ""
         minstr = len(strs[0])
         if len(strs) == 1:
             return strs[0]
         for i in range(1,len(strs)):
-            # print(i,len(strs)-1)
-            # print(minstr,"i=",i)
-            # print(len(strs[i]),"i=",i)
             if len(strs[i])< minstr:
                 minstr = len(strs[i]) 
         for j in range(minstr):
-            # print(j)
             for i in range(1,len(strs)):
-                # print(i,j,len(strs)-1)
-                # print(strs[i][j],strs[i-1][j])
                 if strs[i][j] == strs[i-1][j]:
                     itr = strs[i][j]
-                    # print(i,j,itr)
                 else:
-                    # print(i,j,itr)
                     itr = ""
-                    # print(i,j)
                     break
-            # print(i,j,itr)
             if itr != "":
                 pref += itr
             else: break
