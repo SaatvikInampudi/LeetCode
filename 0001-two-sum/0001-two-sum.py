0f6709bd2974ca39556a13668df993a4c1140370
Saatvik Inampudi <isaatvik@gmail.com>
2025-11-20 20:37:04 -0500

    Time: 3 ms (49.89%), Space: 19 MB (24.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashm = {}
+        hm = {}
         for i in range(len(nums)):
-            hashm[nums[i]] = i
-        
+            hm[nums[i]] = i
+        print(hm)
         for i in range(len(nums)):
-            if target-nums[i] in hashm:
-                if target-nums[i] == nums[i] and hashm[target-nums[i]] == i:
+            if target-nums[i] in hm:
+                if hm[target-nums[i]] == hm[nums[i]] and hm[target-nums[i]]==i:
                     continue
-                return [i,hashm[target-nums[i]]]
+                return [hm[target-nums[i]],i]
+
